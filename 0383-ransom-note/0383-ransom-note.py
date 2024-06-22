5a6f2dba6723f56f8388daf2d8421998fc9221af
David M.J <34282884+martijaram@users.noreply.github.com>
2024-06-21 22:59:52 -0400

    Time: 42 ms (93.64%), Space: 16.6 MB (93.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,25 +1,18 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        table0 = [0]*26
-        for i in ransomNote:
-            table0[ord(i) - 97] += 1
-            
-        table1 = [0]*26
+        table = [0]*26
         for i in magazine:
-            table1[ord(i) - 97] += 1
+            table[ord(i) - 97] += 1
             
-            decision = 0
-            n = 0
-            for i in table1:
-                if i >= table0[n]:
-                    decision += 1
-                n += 1
+        for i in ransomNote:            
+            value = ord(i) - 97
+            table[value] -= 1
             
-            if decision >= 26:
-                return True
+            if (table[value] < 0):
+                return False
                 
                 
             
-        return False
+        return True
             
         
