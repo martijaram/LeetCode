6f5500ce57179674efada260d9ce770703f2d9a2
David M.J <34282884+martijaram@users.noreply.github.com>
2024-06-22 15:58:01 -0400

    Time: 33 ms (68.29%), Space: 16.5 MB (64.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        back0 = 1
+        back1 = 1
+        nextup = 1
+        
+        i = 2
+        while i <= n:
+            nextup = back0 + back1
+            back0 = back1
+            back1 = nextup
+            i += 1
+            
+        return nextup
+            
+            
+        
