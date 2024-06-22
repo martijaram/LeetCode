be51d498d32ca0f002462af293f9fcfb08ead6f5
David M.J <34282884+martijaram@users.noreply.github.com>
2024-06-22 16:14:44 -0400

    Time: 29 ms (87.63%), Space: 16.4 MB (64.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,12 +4,13 @@ class Solution:
         back1 = 1
         nextup = 1
         
-        while n != 1:
+        i = 2
+        while i <= n:
             nextup = back1 + back0
             back0 = back1
             back1 = nextup
             
-            n -= 1
+            i += 1
             
         return nextup
             
