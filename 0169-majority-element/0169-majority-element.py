bf10982a9bf308d130c58bf5f87c92993f9603cd
David M.J <34282884+martijaram@users.noreply.github.com>
2024-06-23 13:49:50 -0400

    Time: 180 ms (21.03%), Space: 18.1 MB (20.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,29 +3,16 @@ class Solution:
         maxm = 0
         num = 0
         
-        counter = [0]*11
+        l = int(len(nums)/2) + 1
+        counter = [0]*l
         
         for i in nums:
-            index = i%10
-            if counter[index] == 0:
-                counter[index] = [[i, 1]]
-                
-                if (maxm < 1):
-                    maxm = 1
-                    num = counter[index][0][0]
-            else:
-                found = False
-                for info in counter[index]:
-                    if (info[0] == i):
-                        info[1] += 1
-                        possiblemax = info[1]
-                        if (possiblemax > maxm):
-                            maxm = possiblemax
-                            num = info[0]
-                            
-                        break
-                    
-                    counter[index].append([i, 1])
+            counter[i%l] += 1
+            new = counter[i%l]
+            
+            if (new > maxm):
+                maxm = new
+                num = i
                 
         return num
             
