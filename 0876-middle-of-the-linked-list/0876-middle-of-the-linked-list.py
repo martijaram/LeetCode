72150715feaafbc27b1fbd5306dfb8e39999caf6
David M.J <34282884+martijaram@users.noreply.github.com>
2024-06-23 22:39:10 -0400

    Time: 37 ms (43.42%), Space: 16.3 MB (99.51%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,28 +6,12 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         speed1 = head
-        speed3 = head
-        hmm = True
+        speed2 = head
         
-        if (not head.next):
-            return head
-        elif (not head.next.next):
-            return head.next
-        elif (not head.next.next.next):
-            return head.next
         
-        while ((speed1 != speed3 and speed1.next != speed3) or hmm):
-            if (not speed3.next):
-                speed3 = head.next.next.next
-            elif (not speed3.next.next):
-                speed3 = head.next.next
-            elif (not speed3.next.next.next):
-                speed3 = head.next
-            else:
-                speed3 = speed3.next.next.next
-            
+        while (speed2 and speed2.next):
+            speed2 = speed2.next.next
             speed1 = speed1.next
-            hmm = False
             
             
         return speed1
